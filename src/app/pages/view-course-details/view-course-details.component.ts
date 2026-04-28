ddcdd02b91154be36cda82d894c2de5c35ca9e2f
mohamedhemdan <mohamedsmsma3s@gmail.com>
2026-04-29 02:28:06 +0300

    fix enroll btn to make user login first if not login

--- src/app/pages/view-course-details/view-course-details.component.ts
+++ src/app/pages/view-course-details/view-course-details.component.ts
@@ -6,8 +6,12 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs'
 import { CourseOverviewComponent } from './course-overview/course-overview.component';
 import { CourseCurriculumComponent } from './course-curriculum/course-curriculum.component';
 import { CourseReviewsComponent } from './course-reviews/course-reviews.component';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import { CourseStore } from '../../core/stores/course.store';
+import { AuthStore } from '../../core/stores/auth.store';
+import { Dialog } from '@angular/cdk/dialog';
+import { LoginComponent } from '../auth/login/login.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-view-course-details',
   imports: [
@@ -28,6 +32,9 @@ export class ViewCourseDetailsComponent implements OnInit {
   courseId = input.required<string>();
   store = inject(CourseStore);
   isInWishlist = signal<boolean>(false);
+  router = inject(Router);
+  authStore = inject(AuthStore);
+  dialog = inject(MatDialog);
   constructor() {
     effect(() => {
       console.log("effect method runs..");
@@ -49,5 +56,19 @@ export class ViewCourseDetailsComponent implements OnInit {
     }else {
       this.store.addToWishlist(this.store.selectedCourse()!);
     }
+  }
+  onEnrollClick() {
+    if (!this.authStore.isAuthenticated()) {
+      const dialogRef = this.dialog.open(LoginComponent, {
+        disableClose: true
+      });
+      dialogRef.afterClosed().subscribe(() => {
+        if (this.authStore.isAuthenticated()) {
+          this.router.navigate(['/checkout', this.courseId()]);
+        }
+      });
+      return;
+    }
+    this.router.navigate(['/checkout', this.courseId()]);
   }
 }
