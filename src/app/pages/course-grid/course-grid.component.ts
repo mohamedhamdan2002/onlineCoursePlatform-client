a12f2db898846a5fb288b749be7f31674507f28e
mohamedhemdan <mohamedsmsma3s@gmail.com>
2026-04-28 14:21:37 +0300

    create course grid

---
+++ src/app/pages/course-grid/course-grid.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { SideBarComponent } from '../../layout/side-bar/side-bar.component';
+import { CourseCardComponent } from "../../shared/components/course-card/course-card.component";
+import { CourseStore } from '../../features/courses/course.store';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+@Component({
+  selector: 'app-course-grid',
+  imports: [
+    MatFormField,
+    MatInput,
+    MatIcon,
+    FormsModule,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    MatPrefix,
+    SideBarComponent,
+    CourseCardComponent,
+    MatPaginator
+],
+  templateUrl: './course-grid.component.html',
+  styleUrl: './course-grid.component.scss',
+})
+export class CourseGridComponent implements OnInit {
+  store = inject(CourseStore);
+  ngOnInit(): void {
+    this.store.loadCategories();
+    this.store.loadCoursePageList();
+  }
+
+  onPageChange(event: PageEvent) {
+    this.store.setPage(event.pageIndex + 1);
+    this.store.setPageSize(event.pageSize);
+    this.store.loadCoursePageList();
+  }
+
+}
