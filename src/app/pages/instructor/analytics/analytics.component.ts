f2a0a02b00c3bdf3df0cb0cd054571cccc62c428
mohamedhemdan <mohamedsmsma3s@gmail.com>
2026-08-29 18:38:01 +0300

    feat: add analytics component

---
+++ src/app/pages/instructor/analytics/analytics.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../../shared/material.module';
+
+@Component({
+  selector: 'app-analytics',
+  imports: [
+    MaterialModule
+  ],
+  templateUrl: './analytics.component.html',
+  styleUrl: './analytics.component.scss',
+})
+export class AnalyticsComponent {
+
+}
