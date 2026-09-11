be57da69479923cc5543d5017d7d5836ef0aa095
mohamedhemdan <mohamedsmsma3s@gmail.com>
2026-09-11 06:38:05 +0300

    feat: add course card skeleton component

---
+++ src/app/shared/components/course-card-skeleton/course-card-skeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card-skeleton',
+  templateUrl: './course-card-skeleton.component.html',
+  styleUrls: ['./course-card-skeleton.component.scss']
+})
+export class CourseCardSkeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
